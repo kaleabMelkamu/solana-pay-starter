fcc9f9766a65bf2c4d59e7b90fd1beeff17675a2
kale <kaleabm18@gmail.com>
2022-06-22 14:15:53 -0700

    wallet connected

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,55 @@
-import React from "react";
-import HeadComponent from "../components/Head";
+import React from 'react';
+import { PublicKey } from '@solana/web3.js';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 
 // Constants
-const TWITTER_HANDLE = "dev_kaleab";
+const TWITTER_HANDLE = 'dev_kaleab';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-    return ( <
-        div className = "App" >
-        <
-        HeadComponent / >
-        <
-        div className = "container" >
-        <
-        header className = "header-container" >
-        <
-        p className = "header" > 😳POKEMON Store😈 < /p>{" "} <
-        p className = "sub-text" > { " " }
-        This is a place where you can buy Pokemon 's{" "} < /
-        p > { " " } <
-        /header>{" "} <
-        main >
-        <
-        img src = "https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif"
-        alt = "emoji" /
-        >
-        <
-        /main>{" "} <
-        div className = "footer-container" >
-        <
-        img alt = "Twitter Logo"
-        className = "twitter-logo"
-        src = "twitter-logo.svg" /
-        >
-        <
-        a className = "footer-text"
-        href = { TWITTER_LINK }
-        target = "_blank"
-        rel = "noreferrer" > { " " } { `built by @${TWITTER_HANDLE}` } { " " } <
-        /a>{" "} < /
-        div > { " " } <
-        /div>{" "} < /
-        div >
-    );
+  // This will fetch the users' public key (wallet address) from any wallet we support
+  const { publicKey } = useWallet();
+
+  const renderNotConnectedContainer = () => (
+    <div>
+      <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+
+      <div className="button-container">
+        <WalletMultiButton className="cta-button connect-wallet-button" />
+      </div>    
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="header-container">
+          <p className="header"> 😳 Pokemon  Store collection 😳</p>
+          <p className="sub-text">This is the place you can buy Pokemon Character </p>
+        </header>
+
+        <main>
+          {/* We only render the connect button if public key doesn't exist */}
+          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
+
+        </main>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src="twitter-logo.svg" />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built by @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
